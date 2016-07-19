fb6eaaf684db1d511bb727917f8180dc0a8a6f68
AgilanSelvaraj <agilan.ks07@gmail.com>
2016-07-19 19:03:45 +0530

    Add files via upload

---
+++ Assignment_3/CorrectUsageOfDataTypes.java
@@ -0,0 +1,10 @@
+package introduction_to_java.Assignment_3;
+
+public class CorrectUsageOfDataTypes {
+	// Two examples of data that cannot be stored in a variable of type int.
+	
+	float a = 5.0f;
+	char b = 'A';
+	
+
+}
